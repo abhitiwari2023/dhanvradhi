342b36567db0d3bccdffc6fee4936a7238135d8c
Abhishek Tiwari <abhishek748932@gmail.com>
2024-07-15 22:34:42 +0530

    mobile view done

--- index.js
+++ index.js
@@ -162,3 +162,12 @@ document.addEventListener('DOMContentLoaded', function() {
         alert('Thank you! Your message has been sent.')
     })
 })
+
+
+// menu button
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+btn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
